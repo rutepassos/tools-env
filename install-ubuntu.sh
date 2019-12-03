90ffc30c350e9e372a594123335ff71b326e21d9
Ana Rute <ana.passos@dcomp.ufs.br>
2019-12-03 11:16:22 -0300

    Update install-ubuntu.sh
    changes

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,6 +1,11 @@
 sudo apt update
 
-sudo apt install curl -y
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+    
 sudo apt install snapd -y
 
 # vscode
@@ -38,13 +43,8 @@ sudo apt install postgresql-contrib -y
 #sudo dpkg -i mongodb-compass_1.15.1_amd64.deb
 
 # docker
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
