7015d04440cd02c8d613545a0779233812662b21
Ana Rute <ana.passos@dcomp.ufs.br>
2019-12-03 11:14:23 -0300

    Update install-ubuntu.sh
    Add chenges

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,8 +1,41 @@
+sudo apt update
+
 sudo apt install curl -y
 sudo apt install snapd -y
 
 # vscode
 sudo snap install --classic code
+
+# sublime
+sudo snap install sublime-text
+
+# zsh
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# plugin zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
+
+#  insominia
+sudo snap install insomnia -y
+
+#  client postgres terminal
+sudo apt install postgresql-contrib -y
+
+# client mysql gui
+#sudo apt install mysql-workbench
+
+# client mongo terminal
+#wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+#sudo apt-get install gnupg -y
+#wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+#echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+#sudo apt-get update
+#sudo apt-get install -y mongodb-org-shell
+
+# cliente mongo gui
+#wget https://downloads.mongodb.com/compass/mongodb-compass_1.15.1_amd64.deb
+#sudo dpkg -i mongodb-compass_1.15.1_amd64.deb
 
 # docker
 sudo apt-get install \
@@ -17,26 +50,13 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
    
-sudo apt-get update
-sudo apt-get install git -y
+sudo apt update
+sudo apt install git -y
 
-sudo apt-get install docker-ce -y
+sudo apt install docker-ce -y
 
 # docker compose
 sudo apt install docker-compose -y
-
-# zsh
-sudo apt install zsh -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# plugin zsg
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
-
-#  insominia
-sudo snap install insomnia -y
-
-#  client postgres
-sudo apt install postgresql-contrib -y
 
 # config docker
 sudo usermod -aG docker $USER
