8ed14dcc9eb5ecb506ce257db8c3c6170b548343
Ana Rute <ana.passos@dcomp.ufs.br>
2019-12-06 10:44:28 -0300

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,33 +1,64 @@
+echo '##########  Executando Update ############'
 sudo apt update
 
-sudo apt install git -y
+if ! [ -x "$(command -v git)" ]; then
+  echo '##########  Instalando Git ############'
+  sudo apt install git -y
+  exit 1
+fi
 
+echo '##########  Instalando Dependencias ############'
 sudo apt install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
-    
+
+echo '##########  Instalando Snap ############'
 sudo apt install snapd -y
 
-# vscode
+
+echo '##########  Instalando VSCODE ############'
 sudo snap install --classic code
 
-# sublime
-sudo snap install sublime-text
+echo '##########  Instalando Sublime ############'
+curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+sudo apt update
+sudo apt install sublime-text
 
-# zsh
+# Abrir sublime pelo terminal
+#sudo ln -s /opt/sublime/sublime_text /usr/bin/subl
+
+echo '##########  Instalando ZSH ############'
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# plugin zsh
+echo '##########  Instalando Plugin ZSH ############'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
 
-#  insominia
+echo '##########  Instalando Insomnia ############'
 sudo snap install insomnia -y
 
-#  client postgres terminal
+echo '##########  Instalando Postgres Client ############'
 sudo apt install postgresql-contrib -y
+
+echo '##########  Instalando Node ############'
+sudo apt install nodejs
+
+echo '##########  Instalando Yarn ############'
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install --no-install-recommends yarn
+
+echo '##########  Instalando Filezilla ############'
+sudo apt-get install filezilla 
+
+if ! [ -x "$(command -v npm)" ]; then
+  echo '##########  Instalando NPM ############'
+  sudo apt install npm
+  exit 1
+fi
 
 # client mysql gui
 #sudo apt install mysql-workbench
@@ -44,22 +75,25 @@ sudo apt install postgresql-contrib -y
 #wget https://downloads.mongodb.com/compass/mongodb-compass_1.15.1_amd64.deb
 #sudo dpkg -i mongodb-compass_1.15.1_amd64.deb
 
-# docker
+echo '##########  Instalando Docker ############'
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
+   $(bionic) \
    stable"
-   
 sudo apt update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-sudo apt install docker-ce -y
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo '##########  Instalando Dcoker Compose ############'
+  sudo apt install docker-compose -y
+  exit 1
+fi
 
-# docker compose
-sudo apt install docker-compose -y
 
-# config docker
+echo '##########  Executando Configuração Dcoker Pós Instalação ############'
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
+
+echo '##########  Reiniciando a Máquina ############'
 reboot
