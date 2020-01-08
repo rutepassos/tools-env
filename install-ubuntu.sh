61bf02bbe72216b47689dd83f2c48dc032e05840
Ana Rute <ana.passos@dcomp.ufs.br>
2020-01-08 16:56:20 -0300

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -4,7 +4,6 @@ sudo apt update
 if ! [ -x "$(command -v git)" ]; then
   echo '##########  Instalando Git ############'
   sudo apt install git -y
-  exit 1
 fi
 
 echo '##########  Instalando Dependencias ############'
@@ -17,73 +16,61 @@ sudo apt install \
 if ! [ -x "$(command -v snap)" ]; then
   echo '##########  Instalando Snap ############'
   sudo apt install snapd -y
-  exit 1
 fi
 
 if ! [ -x "$(command -v code)" ]; then
   echo '##########  Instalando VSCODE ############'
   sudo snap install --classic code
-  exit 1
 fi
 
-if ! [ -x "$(command -v subl)" ]; then
-    echo '##########  Instalando Sublime ############'
-    curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-    sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
-    sudo apt update
-    sudo apt install sublime-text
-    # Abrir sublime pelo terminal
-    #sudo ln -s /opt/sublime/sublime_text /usr/bin/subl
-  exit 1
-fi
-
-if ! [ -x "$(command -v zsh)" ]; then
-    echo '##########  Instalando ZSH ############'
-    sudo apt install zsh -y
-  exit 1
-fi
+# if ! [ -x "$(command -v subl)" ]; then
+#     echo '##########  Instalando Sublime ############'
+#     curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+#     sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+#     sudo apt update
+#     sudo apt install sublime-text
+#     # Abrir sublime pelo terminal
+#     #sudo ln -s /opt/sublime/sublime_text /usr/bin/subl
+# fi
 
 if ! [ -x "$(command -v insomnia)" ]; then
     echo '##########  Instalando Insomnia ############'
     sudo snap install insomnia
-  exit 1
 fi
 
 if ! [ -x "$(command -v psql)" ]; then
     echo '##########  Instalando Postgres Client ############'
     sudo apt install postgresql-contrib -y
-  exit 1
 fi
 
-echo '##########  Instalando Node ############'
-sudo apt install nodejs
+if ! [ -x "$(command -v node)" ]; then
+  echo '##########  Instalando Node ############'
+  sudo apt install nodejs
+fi
+
 
 if ! [ -x "$(command -v yarn)" ]; then
     echo '##########  Instalando Yarn ############'
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
     sudo apt update && sudo apt install --no-install-recommends yarn
-  exit 1
 fi
 
 
 if ! [ -x "$(command -v filezilla)" ]; then
   echo '##########  Instalando Filezilla ############'
   sudo apt-get install filezilla 
-  exit 1
 fi
 
 if ! [ -x "$(command -v npm)" ]; then
   echo '##########  Instalando NPM ############'
   sudo apt install npm
-  exit 1
 fi
 
-#if ! [ -x "$(command -v mysql-workbench)" ]; then
-    #echo '##########  Instalando Mysql Workbench ############'
-    #sudo apt install mysql-workbench
-    #exit 1
-#fi
+# if ! [ -x "$(command -v mysql-workbench)" ]; then
+#     echo '##########  Instalando Mysql Workbench ############'
+#     sudo apt install mysql-workbench
+# fi
 
 
 # client mongo terminal
@@ -102,34 +89,36 @@ fi
     #echo '##########  Instalando Git Kraken ############'
     #wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
     #dpkg -i gitkraken-amd64.deb
-    #exit 1
 #fi
 
-echo '##########  Instalando Oh my zsh ############'
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-echo '##########  Instalando Plugin ZPLUGIN ############'
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
-
-
 if ! [ -x "$(command -v docker)" ]; then
+    
     echo '##########  Instalando Docker ############'
-    sudo apt update
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(bionic) stable"
+    
     sudo apt update
     sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
     if ! [ -x "$(command -v docker-compose)" ]; then
       echo '##########  Instalando Dcoker Compose ############'
       sudo apt install docker-compose -y
-      exit 1
     fi
     
     echo '##########  Executando Configuração Docker Pós Instalação ############'
     sudo usermod -aG docker $USER
     sudo systemctl enable docker
-
-    echo '##########  Reiniciando a Máquina ############'
-    reboot
-  exit 1
 fi
+
+if ! [ -x "$(command -v zsh)" ]; then
+    echo '##########  Instalando ZSH ############'
+    sudo apt install zsh -y
+
+    echo '##########  Instalando Oh my zsh ############'
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+    echo '##########  Instalando Plugin ZPLUGIN ############'
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
+fi
+
+echo '##########  Reinicie a Máquina ############'
