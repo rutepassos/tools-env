d8a1a303b1dc129100ad3dd531b199b4aaa78d36
Ana Rute <ana.passos@dcomp.ufs.br>
2020-01-08 17:20:03 -0300

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -95,7 +95,7 @@ if ! [ -x "$(command -v docker)" ]; then
     
     echo '##########  Instalando Docker ############'
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(bionic) stable"
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
     
     sudo apt update
     sudo apt-get install docker-ce docker-ce-cli containerd.io -y
